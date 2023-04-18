d5f3b71faf3c3dfc8cc6fcdfd56d515b2b17f213
FunkySphere <krauzowiczs42@gmail.com>
2023-04-18 19:49:44 +0200

    refactor progress

--- src/app/database/projects/projects.service.ts
+++ src/app/database/projects/projects.service.ts
@@ -1,60 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Project } from './projectModel';
-import { Observable, Subject } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectsService {
-  constructor(private httpClient: HttpClient) {}
-
-  private url = 'http://localhost:5050/api/projects';
-
-  private projects$: Subject<Project[]> = new Subject();
-  private projects: Project[] = [];
 
   // Read
-  getProjects(): Subject<Project[]> {
-    this.httpClient.get<Project[]>(this.url).subscribe((projects) => {
-      this.projects = projects;
-      this.projects$.next(projects);
-    });
-    return this.projects$;
+  getProjects(): Project[] {
+    if(localStorage.getItem("projects"))
+    {
+      const projects: Project[] = JSON.parse(localStorage.getItem("projects")!);
+      return projects;
+    }
+    return [];
   }
 
-  getProject(projectName: string): Subject<Project> {
-    let project$: Subject<Project> = new Subject();
-    this.httpClient
-      .get<Project>(`${this.url}/${projectName}`)
-      .subscribe((project) => {
-        project$.next(project);
-      });
-    return project$;
+  getProject(projectName: string): Project {
+    if(localStorage.getItem("projects"))
+    {
+      let project: Project;
+      const projects: Project[] = JSON.parse(localStorage.getItem("projects")!);
+      projects.forEach((pr: Project) => {
+        if(projectName === pr.name) {
+          project = pr;
+        }
+      })
+      return project;
+    }
   }
 
   // Create
-  createProject(project: Project): Observable<string> {
+  createProject(project: Project) {
     if (this.hasDuplicates(project)) {
       alert('Project names must be unique');
-      return new Observable();
+      return;
     }
-    let response = this.httpClient.post(this.url, project, {
-      responseType: 'text',
-    });
-    return response;
+    if(localStorage.getItem("projects"))
+    {
+      let projects: Project[] = JSON.parse(localStorage.getItem("projects")!);
+      projects.push(project);
+      localStorage.setItem("projects", JSON.stringify(projects));
+    }
   }
 
   hasDuplicates(project: Project): boolean {
-    let duplicates: boolean = false;
-    this.projects.forEach((pr) => {
-      if (pr.name == project.name) {
-        alert('Project names must be unique');
-        duplicates = true;
-        return;
-      }
-    });
-    return duplicates;
+    let duplicatesExist: boolean = false;
+    if(localStorage.getItem("projects"))
+    {
+      JSON.parse(localStorage.getItem("projects")!).forEach((pr: Project) => {
+        if (pr.name === project.name) {
+          alert('Project names must be unique');
+          duplicatesExist = true;
+          return;
+        }
+      });
+    }
+    return duplicatesExist;
   }
 
   // Update
