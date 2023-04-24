fcf6082cee0ebba420478321e24e938741402650
FunkySphere <krauzowiczs42@gmail.com>
2023-04-24 12:36:43 +0200

    timespan service refactor

--- src/app/database/timespans/timespans.service.ts
+++ src/app/database/timespans/timespans.service.ts
@@ -1,47 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Timespan } from '../timespans/timespanModel';
-import { Observable, Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimespansService {
-  constructor(private httpClient: HttpClient) {}
-
-  // private url = "http://localhost:5050";
-  private url = "http://localhost:5050/api/timespans";
-
-  private timespans$: Subject<Timespan[]> = new Subject();
-
-  // READ
-  private refreshTimespans() {
-    // if in the api each CRUD operation was on the different address, the requests would look like this
-    // this.httpClient.get<Timespan[]>(`${this.url}/api/gettimespans`)
-    this.httpClient.get<Timespan[]>(this.url)
-    .subscribe((timespans) => {
-      this.timespans$.next(timespans);
-    })
-  }
-  getTimespans(): Subject<Timespan[]> {
-    this.refreshTimespans();
-    return this.timespans$;
+  createEmptyTimespansItem() {
+    const newTimespansArray: Timespan[] = [];
+    localStorage.setItem('timespans', JSON.stringify(newTimespansArray));
   }
 
-  // CREATE
-  createTimespan(timespan: Timespan): Observable<string> {
-    // return this.httpClient.post(`${this.url}/api/createtimespan`, timespan, {responseType: "text"})
-    let response = this.httpClient.post(this.url , timespan, {responseType: "text"});
-    response
-    .subscribe({
-      next: () => {
-        // console.log('Timespan saved succesfuly');
-      },
-      error: (err) => {
-        alert('Failed to create a timespan');
-        console.error(err);
-      },
-    });
-    return response;
+  // Read
+  getTimespans(): Timespan[] {
+    if (!localStorage.getItem('timespans')) {
+      this.createEmptyTimespansItem()
+    }
+
+    const timespans: Timespan[] = JSON.parse(localStorage.getItem('timespans')!);
+    return timespans;
+  }
+
+  // Create
+  createTimespan(timespan: Timespan) {
+    if(!localStorage.getItem('timespans')) {
+      this.createEmptyTimespansItem();
+    }
+
+    let timespans: Timespan[] = this.getTimespans();
+    timespans.push(timespan);
+    localStorage.setItem('timespans', JSON.stringify(timespans));
   }
 }
